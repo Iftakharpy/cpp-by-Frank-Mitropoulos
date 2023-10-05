857b35d323bcb129e1fd058c80ba6a4602ff7e4f
Iftakhar Husan <37600593+Iftakharpy@users.noreply.github.com>
2023-10-06 02:29:24 +0300

    Add section 04 exercise

---
+++ section_04/echo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	long long int favourite_number;
+	std::cout << "Enter your favourite number between: ";
+	std::cin >> favourite_number;
+
+	std::cout << "Amazing!! That's my favourite number too!" << std::endl;
+	std::cout << "No really!!, " << favourite_number << " is my favourite number!" << std::endl;
+	return 0;
+}
